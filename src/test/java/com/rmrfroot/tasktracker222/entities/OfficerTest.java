f7bc16ebb7753f96c9d510c7c6debc7fb9bd9275
Brian Frey <brianfrey@csus.edu>
2022-10-05 20:04:13 -0700

    DT-142: http request tests for drill creation. File name: generated-requests.http Tests performed on GET, POST, PUT, DELETE methods in DrillSchedulerController and DrillCollectionController Also included empty test class for Officer class (needs a constructor method before I create the tests). Note: commented out // import com.rmrfroot.tasktracker222.cognitoClasses.CreateUserPool; in MainController because a warning appeared that stated it did not exist.

---
+++ src/test/java/com/rmrfroot/tasktracker222/entities/OfficerTest.java
@@ -0,0 +1,285 @@
+package com.rmrfroot.tasktracker222.entities;
+
+import com.rmrfroot.tasktracker222.Officer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OfficerTest {
+
+    Officer officer = new Officer();
+
+    @Test
+    void setName() {
+
+    }
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void setAddress() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void setBirthday() {
+    }
+
+    @Test
+    void getBirthday() {
+    }
+
+    @Test
+    void setContact_info_id() {
+    }
+
+    @Test
+    void getContact_info_id() {
+    }
+
+    @Test
+    void setGeneral_info_id() {
+    }
+
+    @Test
+    void getGeneral_info_id() {
+    }
+
+    @Test
+    void setTimeline_info_id() {
+    }
+
+    @Test
+    void getTimeline_info_id() {
+    }
+
+    @Test
+    void setRe_dis_dem_info_id() {
+    }
+
+    @Test
+    void getRe_dis_dem_info_id() {
+    }
+
+    @Test
+    void setEducation() {
+    }
+
+    @Test
+    void getEducation() {
+    }
+
+    @Test
+    void setOfficer_info_id() {
+    }
+
+    @Test
+    void getOfficer_info_id() {
+    }
+
+    @Test
+    void setPhone_number() {
+    }
+
+    @Test
+    void getPhone_number() {
+    }
+
+    @Test
+    void setPromotion_eligibility() {
+    }
+
+    @Test
+    void isPromotion_eligibility() {
+    }
+
+    @Test
+    void setHobby() {
+    }
+
+    @Test
+    void getHobby() {
+    }
+
+    @Test
+    void setRank() {
+    }
+
+    @Test
+    void getRank() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setHometown() {
+    }
+
+    @Test
+    void getHometown() {
+    }
+
+    @Test
+    void setMarital_status() {
+    }
+
+    @Test
+    void isMarital_status() {
+    }
+
+    @Test
+    void setAwards_annual() {
+    }
+
+    @Test
+    void getAwards_annual() {
+    }
+
+    @Test
+    void setNumber_of_department() {
+    }
+
+    @Test
+    void getNumber_of_department() {
+    }
+
+    @Test
+    void setAwards_pme() {
+    }
+
+    @Test
+    void getAwards_pme() {
+    }
+
+    @Test
+    void setAward_other() {
+    }
+
+    @Test
+    void getAward_other() {
+    }
+
+    @Test
+    void setDecoration_afam() {
+    }
+
+    @Test
+    void getDecoration_afam() {
+    }
+
+    @Test
+    void setDecoration_afcm() {
+    }
+
+    @Test
+    void getDecoration_afcm() {
+    }
+
+    @Test
+    void setDecoration_msm() {
+    }
+
+    @Test
+    void getDecoration_msm() {
+    }
+
+    @Test
+    void setDecoration_other() {
+    }
+
+    @Test
+    void getDecoration_other() {
+    }
+
+    @Test
+    void setFamilyMap() {
+    }
+
+    @Test
+    void getFamilyMap() {
+    }
+
+    @Test
+    void setWingman() {
+    }
+
+    @Test
+    void getWingman() {
+    }
+
+    @Test
+    void setPersonal_timeline_info() {
+    }
+
+    @Test
+    void getPersonal_timeline_info() {
+    }
+
+    @Test
+    void setProfessional_timeline_info() {
+    }
+
+    @Test
+    void getProfessional_timeline_info() {
+    }
+
+    @Test
+    void setTafmsd() {
+    }
+
+    @Test
+    void getTafmsd() {
+    }
+
+    @Test
+    void setDas() {
+    }
+
+    @Test
+    void getDas() {
+    }
+
+    @Test
+    void setEts() {
+    }
+
+    @Test
+    void getEts() {
+    }
+
+    @Test
+    void setPet() {
+    }
+
+    @Test
+    void getPet() {
+    }
+
+    @Test
+    void setPreferences() {
+    }
+
+    @Test
+    void getPreferences() {
+    }
+}
