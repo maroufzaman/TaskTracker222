83a00fe5cff0aa39f29ae45e7167323ebf1d3b5b
tmaduchukwu <tmaduchukwu22@gmail.com>
2022-10-19 21:15:50 -0700

    DT-155: Created a post controller for edits on Drills.

--- src/main/java/com/rmrfroot/tasktracker222/services/DrillScheduleService.java
+++ src/main/java/com/rmrfroot/tasktracker222/services/DrillScheduleService.java
@@ -1,9 +1,12 @@
 package com.rmrfroot.tasktracker222.services;
 
 import com.rmrfroot.tasktracker222.entities.DrillSchedules;
+import com.rmrfroot.tasktracker222.entities.Users;
 
 public interface DrillScheduleService {
 
     public DrillSchedules findDrillSchedulesById(int id);
     public void save(DrillSchedules drillSchedules);
+
+    DrillSchedules update(int id, DrillSchedules drillSchedules);
 }
