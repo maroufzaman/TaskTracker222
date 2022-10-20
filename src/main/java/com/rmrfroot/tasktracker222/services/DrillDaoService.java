52a9925ce1236a963290523d0450162c650602a0
tmaduchukwu <tmaduchukwu22@gmail.com>
2022-10-19 22:21:04 -0700

    DT-221: Added input sanitization for drill creation.

--- src/main/java/com/rmrfroot/tasktracker222/services/DrillDaoService.java
+++ src/main/java/com/rmrfroot/tasktracker222/services/DrillDaoService.java
@@ -2,6 +2,7 @@ package com.rmrfroot.tasktracker222.services;
 
 import com.rmrfroot.tasktracker222.entities.deprecated.Drill;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //drill dao service
@@ -18,5 +19,8 @@ public interface DrillDaoService {
 
     public Drill update( int id,Drill drill);
 
+    public Boolean hasDrillData(String title);
 
+    public void registerDrillToDatabase(String name, String event_title, String start_date, String deadline_date,
+                                 String location, String title, String admin_name, String officer_email, String created_timestamp, String note);
 }
